8230a375d1db5e8e6c4a5afddc3e4f9db09d0426
badarmasood <badarmasood99@gmail.com>
2021-11-10 00:15:09 +0500

    Discount App

--- App.js
+++ App.js
@@ -1,44 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
-  const [getText,setText]=React.useState(0)
-  const [getDisText,setDisText]=React.useState(0)
+  const [getText, setText] = React.useState(0);
+  const [getDisText, setDisText] = React.useState(0);
 
-  const [SaveText,setSaveText]=React.useState(0)
+  const [Save, setSave] = React.useState(0);
+  const [FinalPrice, setFinalPrice] = React.useState(0);
 
-  let Discount=()=>{
-    setDisText((getText*getDisText)/100)
-  }
-  
-  
+  let Discount = () => {
+    setSave((getText * getDisText) / 100);
+  };
+
+  let SaveAmount = () => {
+    setFinalPrice(getText - Save);
+  };
 
   return (
     <View style={styles.container}>
+      <View style={styles.playersInfo}>
+        <Text style={styles.playersText}>DISCOUNT APP</Text>
+      </View>
 
-    <View style={styles.playersInfo}>
-    <Text style={styles.playersText}>DISCOUNT APP</Text>
-      <StatusBar style="auto" />
-    </View>
-
-      <TextInput style={styles.input}
-         onChangeText={(getText)=>setText(getText)}
-        
+      <TextInput
+        style={styles.input}
+        onChangeText={(getText) => setText(getText)}
         placeholder="Enter the Price"
         keyboardType="numeric"
       />
 
-      <TextInput style={styles.input}
-        onChangeText={(getDisText)=>setDisText(getDisText)}
+      <TextInput
+        style={styles.input}
+        onChangeText={(getDisText) => setDisText(getDisText)}
         placeholder="Discount %"
         keyboardType="numeric"
       />
 
-      <Text style={styles.text}>You Save : {SaveText} </Text>
-      <Text style={styles.text}>Final Price : {getText-getDisText} </Text>
+      <Text style={styles.text}>You Save : {Save} </Text>
+
+      <Text style={styles.text}>Final Price : {FinalPrice} </Text>
+
       <TouchableOpacity style={styles.button} onPress={Discount}>
-        <Text  style={{color:'white',fontSize:20,fontWeight:'bold',}}>Calculate</Text>
+        <Text style={{ color: "white", fontSize: 20, fontWeight: "bold" }}>
+          Apply Discount
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={SaveAmount}>
+        <Text style={{ color: "white", fontSize: 20, fontWeight: "bold" }}>
+          Final Price
+        </Text>
       </TouchableOpacity>
     </View>
   );
@@ -47,51 +64,48 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent:'center',
-    padding:'2%'
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    padding: "2%",
   },
   input: {
     height: 50,
-    marginTop:15,
-    marginBottom:15,
+    marginTop: 15,
+    marginBottom: 15,
     borderWidth: 2,
     padding: 10,
-
   },
-  button:{
-    marginTop:50,
-    alignItems:'center',
-    backgroundColor:'#51Af75',
-    padding:10,
-    justifyContent:'center',
-    height:50,
-    
-    
+  button: {
+    marginTop: 20,
+    alignItems: "center",
+    backgroundColor: "#51Af75",
+    padding: 10,
+    justifyContent: "center",
+    height: 50,
+    borderWidth: 1,
   },
   playersInfo: {
-    
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop:'10%',
-    paddingTop:'5%',
-    paddingBottom:'5%',
-   
-    marginBottom:'1%',   
+    height: 50,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+    // marginTop: "10%",
+    // paddingTop: "5%",
+    // paddingBottom: "5%",
+
+    // marginBottom: "1%",
   },
-  playersText:{
-    fontSize:24,
-    fontWeight:`bold`,
-    color:'white'
+  playersText: {
+    fontSize: 24,
+    fontWeight: `bold`,
+    color: "white",
   },
-  text:{
-    marginTop:5,
-    padding:15,
+  text: {
+    marginTop: 5,
+    padding: 15,
     backgroundColor: "#000",
-    color:"#fff",
-    textAlign:"center",
-    fontWeight:'bold',
-    
-  }
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
 });
